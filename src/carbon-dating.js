db75ebb2dab7c69dc02d9571d27b408818209d43
Anastasia Pashkevich <Nastyapashkevich10@gmail.com>
2021-03-23 00:26:46 +0200

    feat: add solutions to the tasks "count-cats" and "carbon-dating"

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+module.exports = function dateSample(sampleActivity = 'no-value') {
+  const MODERN_ACTIVITY = 15; 
+  const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if(sampleActivity === 'no-value') return false;
+  if(typeof sampleActivity !== 'string') return false;
+
+  const sampleActivityNum = parseFloat(sampleActivity);
+  if(isNaN(sampleActivityNum)) return false;
+  if(sampleActivityNum > MODERN_ACTIVITY) return false;
+  if(sampleActivityNum === 0 || sampleActivityNum < 0) return false;
+
+  const k = Math.LN2 / HALF_LIFE_PERIOD;
+  
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityNum) / k);
 };
