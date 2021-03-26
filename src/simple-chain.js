4f48e2e76907176d1480ebc05f4b060b0fc71e53
Anastasia Pashkevich <Nastyapashkevich10@gmail.com>
2021-03-26 22:39:40 +0200

    feat: add solution to the task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  currChain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.currChain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.currChain.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(this._checkIntegerNumber(position)) {
+      if(position < 1 || position > this.getLength()) {
+        this._deleteChain();
+        throw new Error('Error!');
+      }
+      const index = position - 1;
+      this.currChain.splice(index, 1);
+      return this;
+    }
+    this._deleteChain();
+    throw new Error('Error!');
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.currChain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const chain = this._getChain();
+    this._deleteChain();
+    return chain;
+  },
+  _deleteChain() {
+    this.currChain.length = 0;
+  },
+  _checkIntegerNumber(number) {
+    return typeof number === 'number' && (number ^ 0) === number;
+  },
+  _getChain() {
+    return this.currChain.join('~~');
   }
 };
 
